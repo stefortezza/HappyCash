6682fded7e1e565ed181c6ffdda4963befb36c4c
stefortezza <stefano.fortezza98@hotmail.it>
2025-05-23 15:52:37 +0200

    commit environment CORRETTO

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiURL:'http://localhost:8080/',
+    apiURL:'http://api.happycash.it:8080/api/',
     mapboxToken: 'pk.eyJ1Ijoic3RlZm9ydGV6emEiLCJhIjoiY205anptNjVmMGhiODJqcXY2a2x6eXp4bCJ9.VvPOk79MLhCCcp7kIqVFrw'
 };
