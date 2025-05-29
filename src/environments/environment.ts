53c9e0f5446cd9cb0f30b1ece5b05d246b47c962
stefortezza <stefano.fortezza98@hotmail.it>
2025-05-29 11:18:21 +0200

    corrretto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiURL:'http://api.happycash.it/',
+    apiURL:'https://api.happycash.it/',
     mapboxToken: 'pk.eyJ1Ijoic3RlZm9ydGV6emEiLCJhIjoiY205anptNjVmMGhiODJqcXY2a2x6eXp4bCJ9.VvPOk79MLhCCcp7kIqVFrw'
 };
